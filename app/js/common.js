12d88bb1782384446ccfc361502f030713bc4685
Max <napavoz666@ya.ru>
2016-06-26 22:55:18 +0300

    main page, let's beging

---
+++ app/js/common.js
@@ -0,0 +1,74 @@
+//'use strict';
+//
+//var getKitchenApp = angular.module('getKitchenApp', [
+//    'ngRoute',
+//    'kitchenControllers'
+//]);
+//
+//getKitchenApp.config(['$routeProvider',
+//    function($routeProvider){
+//        $routeProvider
+//            .when('/kitchens', {
+//                templateUrl: './../html/pages/main.html',
+//                controller: 'KitchenUsCtrl'
+//            })
+//            .otherwise({
+//                redirectTo: '/kitchens'
+//            });
+//    }
+//]);
+//'use strict';
+//
+//var getKitchenApp = angular.module('getKitchenApp');
+//
+//getKitchenApp.controller('')
+
+//$(document).ready(function(){
+//    console.log('hi max');
+//});
+//'use strict';
+//var getKitchenApp = angular.module('getKitchenApp', []);
+
+
+// Define the `PhoneListController` controller on the `phonecatApp` module
+//getKitchenApp.controller('kitchenListControllers', function kitchenListControllers($scope) {
+//
+//    $scope.kitchens = [
+//        {
+//            name: 'Nexus S',
+//            snippet: 'Fast just got faster with Nexus S.'
+//        }, {
+//            name: 'Motorola XOOM™ with Wi-Fi',
+//            snippet: 'The Next, Next Generation tablet.'
+//        }, {
+//            name: 'MOTOROLA XOOM™',
+//            snippet: 'The Next, Next Generation tablet.'
+//        }
+//    ];
+//});
+//'use strict';
+//
+//// Define the `phonecatApp` module
+//angular.module('getKitchenApp', [
+//    'ngRoute',
+//    'kitchenControllers'
+//]);
+//
+//'use strict';
+//
+//angular
+//    .module('getKitchenApp')
+//    .config(['$locationProvider' ,'$routeProvider',
+//        function config($locationProvider, $routeProvider) {
+//            $locationProvider.hashPrefix('');
+//
+//            $routeProvider
+//                .when('/phones', {
+//                    templateUrl: './../html/pages/main.html'
+//                })
+//                .otherwise('/phones');
+//        }
+//    ]);
+
+//'use strict';
+//angular.module('getKitchenApp', []);
---
+++ gruntfile.js
@@ -0,0 +1,78 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+        concat: {
+            dist: {
+                src: [
+                    './app/js-plugins/jquery-1.11.3.min.js',
+                    './app/js/**/**.js'
+                    ],
+                dest: 'userFolder/js/compress.js'
+            }
+        },
+        uglify: {
+            compress: {
+                files: {
+                    'userFolder/js/compress.js': ['userFolder/js/compress.js']
+                },
+                options: {
+                    mangle: false
+                }
+            }
+        },
+        slim: {                              // Task
+            dist: {                            // Target
+                files: {                         // Dictionary of files
+                    './userFolder/html/index.html': './app/slim/index.slim',
+                    './userFolder/html/pages/catalog.html': './app/slim/pages/catalog.slim',
+                    './userFolder/html/pages/product-card.html': './app/slim/pages/product-card.slim',
+                    './userFolder/html/pages/campaign.html': './app/slim/pages/campaign.slim',
+                    './userFolder/html/pages/typical-page.html': './app/slim/pages/typical-page.slim'
+                }
+            }
+        },
+        sass: {                              // Task
+            dist: {                            // Target
+                options: {                       // Target options
+                    style: 'expanded'
+                },
+                files: {                         // Dictionary of files
+                    './userFolder/css/main.css': './app/sass/main.scss'       // 'destination': 'source'
+                }
+            }
+        },
+        sprite:{
+            all:{
+                src: './userFolder/images/sprites/**/**/**/*.png',
+                dest: './userFolder/images/sprites.png',
+                destCss: './app/sass/template/generated/_sprites.scss',
+                cssTemplate: './app/grunt/templates/sprites.scss.handlebars',
+                imgPath: './../../userFolder/images/sprites.png',
+                padding: 2
+            }
+        },
+        watch: {
+            files: ['./app/sass/**/**/*.scss', './app/slim/**/**/*.slim'],
+            // 'app/js/**/**/*.js',      //, './userFolder/images/sprites/**/**/**/*.png'
+            tasks: ['slim', 'sass'/*, 'concat', 'sprite'*/],
+            options: {
+                event: ['changed', 'added', 'deleted'],
+                spawn: false
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.loadNpmTasks('grunt-slim');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-spritesmith');
+
+    grunt.registerTask('js-compile', [
+        'concat'
+        //'uglify'
+    ]);
+
+    grunt.registerTask('default', ['js-compile', 'slim', 'sass', 'watch']);
+};
